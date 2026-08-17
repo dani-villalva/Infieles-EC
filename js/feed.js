31680fb0a1fe0fe70208f54597c67bf28b7e4dda
dani-villalva <72467317+dani-villalva@users.noreply.github.com>
2026-08-17 13:01:18 -0500

    Refactor renderFeed function for new post structure

--- js/feed.js
+++ js/feed.js
@@ -1,34 +1,29 @@
-function renderFeed(historiasFiltradas) {
+function renderFeed(historias) {
     const feed = document.getElementById('feedContainer');
     feed.innerHTML = '';
 
-    historiasFiltradas.forEach((historia, index) => {
+    historias.forEach(h => {
         const post = document.createElement('div');
-        post.className = 'post-card';
-        post.onclick = () => openReNote(historia.id);
+        post.className = 'secret-card';
+        post.onclick = () => openDetail(h);
         
+        let imgHtml = h.imagen ? `<img src="${h.imagen}" class="sc-img">` : '';
+
         post.innerHTML = `
-            <img src="${historia.imagen}" alt="Historia" class="post-img">
-            <div class="post-content">
-                <div class="post-title">${historia.texto}</div>
-                <div class="post-footer">
-                    <div class="post-author">
-                        <img src="${historia.avatar}" alt="Avatar">
-                        <span>${historia.autor}</span>
-                    </div>
-                    <div class="post-likes">
-                        <i class="fa-solid fa-heart"></i> ${historia.likes}
-                    </div>
+            <div class="sc-header">
+                <span class="sc-badge">${h.tipo}</span>
+                <div class="sc-meta">${h.fecha} <i class="fa-solid fa-ellipsis-vertical"></i></div>
+            </div>
+            ${imgHtml}
+            <div class="sc-body">${h.texto}</div>
+            <div class="sc-footer">
+                <div class="sc-reactions">
+                    <span>🔥 ${h.likes}</span>
+                    <span>😢 ${h.sads}</span>
                 </div>
+                <span><i class="fa-solid fa-comment"></i> ${h.comentarios}</span>
             </div>
         `;
         feed.appendChild(post);
-
-        if ((index + 1) % 4 === 0) {
-            const ad = document.createElement('div');
-            ad.className = 'ad-banner-feed';
-            ad.innerHTML = `<img src="https://via.placeholder.com/300x400/1E1E23/FDA1CB?text=+ANUNCIO+" alt="Ad">`;
-            feed.appendChild(ad);
-        }
     });
 }
