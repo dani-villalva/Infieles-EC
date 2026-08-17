97a96f8a7c66c33f25a6d560e34622e4f97ebdbc
dani-villalva <72467317+dani-villalva@users.noreply.github.com>
2026-08-17 12:27:02 -0500

    Refactor renderFeed to improve post display

--- js/feed.js
+++ js/feed.js
@@ -4,20 +4,30 @@ function renderFeed(historiasFiltradas) {
 
     historiasFiltradas.forEach((historia, index) => {
         const post = document.createElement('div');
-        post.className = 'post';
+        post.className = 'post-card';
+        post.onclick = () => openReNote(historia.id);
+        
         post.innerHTML = `
-            <div class="post-header">📍 ${historia.ciudad} &nbsp;|&nbsp; 🚩 ${historia.tipo}</div>
-            <div class="post-body">"${historia.texto}"</div>
-            <button class="btn-renote" onclick="openReNote(${historia.id})">
-                <i class="fa-regular fa-comment-dots"></i> Abrir ReNOTE
-            </button>
+            <img src="${historia.imagen}" alt="Historia" class="post-img">
+            <div class="post-content">
+                <div class="post-title">${historia.texto}</div>
+                <div class="post-footer">
+                    <div class="post-author">
+                        <img src="${historia.avatar}" alt="Avatar">
+                        <span>${historia.autor}</span>
+                    </div>
+                    <div class="post-likes">
+                        <i class="fa-solid fa-heart"></i> ${historia.likes}
+                    </div>
+                </div>
+            </div>
         `;
         feed.appendChild(post);
 
         if ((index + 1) % 4 === 0) {
             const ad = document.createElement('div');
-            ad.className = 'post ad-banner';
-            ad.innerHTML = `<img src="https://via.placeholder.com/800x250/1E1E23/FDA1CB?text=++ANUNCIO+PATROCINADO++" alt="Ad">`;
+            ad.className = 'ad-banner-feed';
+            ad.innerHTML = `<img src="https://via.placeholder.com/300x400/1E1E23/FDA1CB?text=+ANUNCIO+" alt="Ad">`;
             feed.appendChild(ad);
         }
     });
